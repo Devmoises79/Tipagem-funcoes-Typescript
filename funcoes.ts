16d62e6e9ceb1a708bf1c47cf388b82d8c80a6d9
Devmoises79 <devmoisesss@gmail.com>
2025-01-27 13:52:16 -0300

    first commit

---
+++ funcoes.ts
@@ -0,0 +1,27 @@
+// Tipagem de funções
+
+function calculaArea(base: number, altura: number): number {
+    return base * altura;
+}
+
+const calculaArea2 = (base: number, altura: number): number => base * altura;
+
+
+function somar(...numeros: number[]): void {
+    //numeros.reduce()
+    console.log(numeros); 
+}
+
+// retorno é o void
+
+
+function teste(): string | number {
+    if(10 > 5) {
+        return 'Dez maior que cinco'
+    } else {
+        return 5
+    }
+}
+
+
+const resultadoTeste = teste();
